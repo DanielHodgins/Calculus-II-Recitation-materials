06e1be6094bc9a1eb1be140507bd4fb95a5ad34e
Daniel Hodgins <121420574+DanielHodgins@users.noreply.github.com>
2026-08-21 09:13:59 -0500

    Add files via upload

--- docs/lunr-pretext-search-index.js
+++ docs/lunr-pretext-search-index.js
@@ -7,7 +7,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet",
   "number": "1",
   "title": "Week 1: Integration by Parts",
-  "body": " Week 1: Integration by Parts     To be able to use the L.I.A.T.E. (Log, inverse, algebraic, Trig, exponential) rule    To be able to use the integration by parts formula or the tabular method    To be able to identify and       Integration by Parts Formula  The formula for integration by parts can be derived from the product rule for derivatives which gives the following formula:      Instructions: Evaluate each integral using integration by parts. Show all of your work. When appropriate, use the L.I.A.T.E. rule to help you choose and .   Exponential by Trigonometric      What was challenging? Note what you wonder or notice as you solve the problem.        Logarithmic by Algebraic      What was challenging? Note what you wonder or notice as you solve the problem.     Inverse Trigonometric      What was challenging? Note what you wonder or notice as you solve the problem.        Exponential Composed with Algebraic      What was challenging? Note what you wonder or notice as you solve the problem.     Problem 5      What was challenging? Note what you wonder or notice as you solve the problem.      Whole Class Discussion        How did you decide what to choose for and ?    How did the L.I.A.T.E. rule help you, and where did it not give you an obvious answer?    What makes a choice of and effective?    What advice would you give someone who is learning integration by parts for the first time?      "
+  "body": " Week 1: Integration by Parts     To be able to use the L.I.A.T.E. (Log, inverse, algebraic, Trig, exponential) rule    To be able to use the integration by parts formula or the tabular method    To be able to identify and       Integration by Parts Formula  The formula for integration by parts can be derived from the product rule for derivatives which gives the following formula:      Instructions: Evaluate each integral using integration by parts. Show all of your work. When appropriate, use the L.I.A.T.E. rule to help you choose and .   Exponential by Trigonometric      What was challenging? Note what you wonder or notice as you solve the problem.        Logarithmic by Algebraic      What was challenging? Note what you wonder or notice as you solve the problem.     Inverse Trigonometric      What was challenging? Note what you wonder or notice as you solve the problem.        Exponential Composed with Algebraic      What was challenging? Note what you wonder or notice as you solve the problem.      Whole Class Discussion        How did you decide what to choose for and ?    How did the L.I.A.T.E. rule help you, and where did it not give you an obvious answer?    What makes a choice of and effective?    What advice would you give someone who is learning integration by parts for the first time?      "
 },
 {
   "id": "ws-week-1-day-1-2",
@@ -64,22 +64,85 @@ var ptx_lunr_docs = [
   "body": " Exponential Composed with Algebraic      What was challenging? Note what you wonder or notice as you solve the problem.   "
 },
 {
-  "id": "ws-week-1-day-1-6-2",
-  "level": "2",
-  "url": "ws-week-1-day-1.html#ws-week-1-day-1-6-2",
-  "type": "Worksheet Exercise",
-  "number": "1.5",
-  "title": "Problem 5.",
-  "body": " Problem 5      What was challenging? Note what you wonder or notice as you solve the problem.   "
-},
-{
   "id": "ws-week-1-day-2",
   "level": "1",
   "url": "ws-week-1-day-2.html",
   "type": "Worksheet",
   "number": "2",
-  "title": "Week 1 day 2",
-  "body": " Week 1 day 2    "
+  "title": "Week 1:Trigonometric Integrals",
+  "body": " Week 1:Trigonometric Integrals     To be able to evaulate integrals where the integrand is a product of a power of sine and a power of cosine    To be able to evaulate integrals where the integrand is a product of a power of secant and a power of tangent.    ...      formulas                                        Instructions: Instructions: Evaluate each integral using the appropriate trigonometric identity.       What was challenging? Note what you wonder or notice as you solve the problem.         What was challenging? Note what you wonder or notice as you solve the problem.            What was challenging? Note what you wonder or notice as you solve the problem.         What was challenging? Note what you wonder or notice as you solve the problem.            What was challenging? Note what you wonder or notice as you solve the problem.         What was challenging? Note what you wonder or notice as you solve the problem.      Whole Class Discussion        What identities are useful for integrating power of trig functions?    How do you decide which trigonometric identity to use when evaluating an integral involving a product of trig functions?    What is the purpose of using the Pythagorean identities in these integrals?    What would happen if you chose the wrong factor to save? Can you still recover from that choice?      "
+},
+{
+  "id": "ws-week-1-day-2-2",
+  "level": "2",
+  "url": "ws-week-1-day-2.html#ws-week-1-day-2-2",
+  "type": "Objectives",
+  "number": "2",
+  "title": "",
+  "body": "   To be able to evaulate integrals where the integrand is a product of a power of sine and a power of cosine    To be able to evaulate integrals where the integrand is a product of a power of secant and a power of tangent.    ...    "
+},
+{
+  "id": "Formula2",
+  "level": "2",
+  "url": "ws-week-1-day-2.html#Formula2",
+  "type": "Remark",
+  "number": "2.1",
+  "title": "formulas.",
+  "body": " formulas                                    "
+},
+{
+  "id": "ws-week-1-day-2-4-2",
+  "level": "2",
+  "url": "ws-week-1-day-2.html#ws-week-1-day-2-4-2",
+  "type": "Worksheet Exercise",
+  "number": "2.1",
+  "title": "",
+  "body": "     What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-1-day-2-4-3",
+  "level": "2",
+  "url": "ws-week-1-day-2.html#ws-week-1-day-2-4-3",
+  "type": "Worksheet Exercise",
+  "number": "2.2",
+  "title": "",
+  "body": "     What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-1-day-2-5-1",
+  "level": "2",
+  "url": "ws-week-1-day-2.html#ws-week-1-day-2-5-1",
+  "type": "Worksheet Exercise",
+  "number": "2.3",
+  "title": "",
+  "body": "     What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-1-day-2-5-2",
+  "level": "2",
+  "url": "ws-week-1-day-2.html#ws-week-1-day-2-5-2",
+  "type": "Worksheet Exercise",
+  "number": "2.4",
+  "title": "",
+  "body": "     What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-1-day-2-6-1",
+  "level": "2",
+  "url": "ws-week-1-day-2.html#ws-week-1-day-2-6-1",
+  "type": "Worksheet Exercise",
+  "number": "2.5",
+  "title": "",
+  "body": "     What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-1-day-2-6-2",
+  "level": "2",
+  "url": "ws-week-1-day-2.html#ws-week-1-day-2-6-2",
+  "type": "Worksheet Exercise",
+  "number": "2.6",
+  "title": "",
+  "body": "     What was challenging? Note what you wonder or notice as you solve the problem.   "
 }
 ]
 
