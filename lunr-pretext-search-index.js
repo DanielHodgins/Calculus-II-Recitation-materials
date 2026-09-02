1d5ceb96db6d5d9760e70c208bb39407a19acc0c
Daniel Hodgins <121420574+DanielHodgins@users.noreply.github.com>
2026-09-02 09:31:45 -0500

    Add files via upload
    fix section->worksheet

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,419 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "ws-week-1-day-1",
+  "level": "1",
+  "url": "ws-week-1-day-1.html",
+  "type": "Worksheet",
+  "number": "1",
+  "title": "Week 1: Integration by Parts",
+  "body": " Week 1: Integration by Parts     To be able to use the L.I.A.T.E. (Log, inverse, algebraic, Trig, exponential) rule    To be able to use the integration by parts formula or the tabular method    To be able to identify and       Integration by Parts Formula  The formula for integration by parts can be derived from the product rule for derivatives which gives the following formula:      Instructions: Evaluate each integral using integration by parts. Show all of your work. When appropriate, use the L.I.A.T.E. rule to help you choose and .   Exponential by Trigonometric      What was challenging? Note what you wonder or notice as you solve the problem.        Logarithmic by Algebraic      What was challenging? Note what you wonder or notice as you solve the problem.     Inverse Trigonometric      What was challenging? Note what you wonder or notice as you solve the problem.        Exponential Composed with Algebraic      What was challenging? Note what you wonder or notice as you solve the problem.      Whole Class Discussion        How did you decide what to choose for and ?    How did the L.I.A.T.E. rule help you, and where did it not give you an obvious answer?    What makes a choice of and effective?    What advice would you give someone who is learning integration by parts for the first time?      "
+},
+{
+  "id": "ws-week-1-day-1-2",
+  "level": "2",
+  "url": "ws-week-1-day-1.html#ws-week-1-day-1-2",
+  "type": "Objectives",
+  "number": "1",
+  "title": "",
+  "body": "   To be able to use the L.I.A.T.E. (Log, inverse, algebraic, Trig, exponential) rule    To be able to use the integration by parts formula or the tabular method    To be able to identify and     "
+},
+{
+  "id": "Formula",
+  "level": "2",
+  "url": "ws-week-1-day-1.html#Formula",
+  "type": "Remark",
+  "number": "1.1",
+  "title": "Integration by Parts Formula.",
+  "body": " Integration by Parts Formula  The formula for integration by parts can be derived from the product rule for derivatives which gives the following formula:  "
+},
+{
+  "id": "ws-week-1-day-1-4-2",
+  "level": "2",
+  "url": "ws-week-1-day-1.html#ws-week-1-day-1-4-2",
+  "type": "Worksheet Exercise",
+  "number": "1.1",
+  "title": "Exponential by Trigonometric.",
+  "body": " Exponential by Trigonometric      What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-1-day-1-5-1",
+  "level": "2",
+  "url": "ws-week-1-day-1.html#ws-week-1-day-1-5-1",
+  "type": "Worksheet Exercise",
+  "number": "1.2",
+  "title": "Logarithmic by Algebraic.",
+  "body": " Logarithmic by Algebraic      What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-1-day-1-5-2",
+  "level": "2",
+  "url": "ws-week-1-day-1.html#ws-week-1-day-1-5-2",
+  "type": "Worksheet Exercise",
+  "number": "1.3",
+  "title": "Inverse Trigonometric.",
+  "body": " Inverse Trigonometric      What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-1-day-1-6-1",
+  "level": "2",
+  "url": "ws-week-1-day-1.html#ws-week-1-day-1-6-1",
+  "type": "Worksheet Exercise",
+  "number": "1.4",
+  "title": "Exponential Composed with Algebraic.",
+  "body": " Exponential Composed with Algebraic      What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-1-day-2",
+  "level": "1",
+  "url": "ws-week-1-day-2.html",
+  "type": "Worksheet",
+  "number": "2",
+  "title": "Week 1: Trig Integrals",
+  "body": " Week 1: Trig Integrals       To be able to evaulate integrals where the integrand is a product of a power of sine and a power of cosine    To be able to evaulate integrals where the integrand is a product of a power of secant and a power of tangent.      formulas                       Instructions: Instructions: Evaluate each integral using the appropriate trigonometric identity.          What was challenging? Note what you wonder or notice as you solve the problem.         What was challenging? Note what you wonder or notice as you solve the problem.            What was challenging? Note what you wonder or notice as you solve the problem.         What was challenging? Note what you wonder or notice as you solve the problem.            What was challenging? Note what you wonder or notice as you solve the problem.         What was challenging? Note what you wonder or notice as you solve the problem.         Whole Class Discussion        What identities are useful for integrating power of trig functions?    How do you decide which trigonometric identity to use when evaluating an integral involving a product of trig functions?    What is the purpose of using the Pythagorean identities in these integrals?    What would happen if you chose the wrong factor to save? Can you still recover from that choice?      "
+},
+{
+  "id": "ws-week-1-day-2-2-1",
+  "level": "2",
+  "url": "ws-week-1-day-2.html#ws-week-1-day-2-2-1",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "   To be able to evaulate integrals where the integrand is a product of a power of sine and a power of cosine    To be able to evaulate integrals where the integrand is a product of a power of secant and a power of tangent.    "
+},
+{
+  "id": "Formula2",
+  "level": "2",
+  "url": "ws-week-1-day-2.html#Formula2",
+  "type": "Remark",
+  "number": "2.1",
+  "title": "formulas.",
+  "body": " formulas                     "
+},
+{
+  "id": "ws-week-1-day-2-3-1",
+  "level": "2",
+  "url": "ws-week-1-day-2.html#ws-week-1-day-2-3-1",
+  "type": "Worksheet Exercise",
+  "number": "2.1",
+  "title": "",
+  "body": "     What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-1-day-2-3-2",
+  "level": "2",
+  "url": "ws-week-1-day-2.html#ws-week-1-day-2-3-2",
+  "type": "Worksheet Exercise",
+  "number": "2.2",
+  "title": "",
+  "body": "     What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-1-day-2-4-1",
+  "level": "2",
+  "url": "ws-week-1-day-2.html#ws-week-1-day-2-4-1",
+  "type": "Worksheet Exercise",
+  "number": "2.3",
+  "title": "",
+  "body": "     What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-1-day-2-4-2",
+  "level": "2",
+  "url": "ws-week-1-day-2.html#ws-week-1-day-2-4-2",
+  "type": "Worksheet Exercise",
+  "number": "2.4",
+  "title": "",
+  "body": "     What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-1-day-2-5-1",
+  "level": "2",
+  "url": "ws-week-1-day-2.html#ws-week-1-day-2-5-1",
+  "type": "Worksheet Exercise",
+  "number": "2.5",
+  "title": "",
+  "body": "     What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-1-day-2-5-2",
+  "level": "2",
+  "url": "ws-week-1-day-2.html#ws-week-1-day-2-5-2",
+  "type": "Worksheet Exercise",
+  "number": "2.6",
+  "title": "",
+  "body": "     What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-2-day-1",
+  "level": "1",
+  "url": "ws-week-2-day-1.html",
+  "type": "Worksheet",
+  "number": "3",
+  "title": "Week 2: Trig Sub",
+  "body": " Week 2: Trig Sub       Identify when trigonometric substitution is an appropriate integration technique.    Apply trigonometric substitution and identities to simplify and evaluate integrals.    Interpret and verify solutions by converting back to the original variable.      formulas      Expression  Trig Substitution  Angle Restriction  Identity                                                  Instructions: Instructions: Evaluate each integral. Show all work and clearly indicate any substitutions or identities used.          What was challenging? Note what you wonder or notice as you solve the problem.         What was challenging? Note what you wonder or notice as you solve the problem.            What was challenging? Note what you wonder or notice as you solve the problem.         What was challenging? Note what you wonder or notice as you solve the problem.            What was challenging? Note what you wonder or notice as you solve the problem.         What was challenging? Note what you wonder or notice as you solve the problem.         Whole Class Discussion      How do you decide between , , and ?    How is trig substitution similar to and different from ordinary -substitution?      "
+},
+{
+  "id": "ws-week-2-day-1-2-1",
+  "level": "2",
+  "url": "ws-week-2-day-1.html#ws-week-2-day-1-2-1",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "   Identify when trigonometric substitution is an appropriate integration technique.    Apply trigonometric substitution and identities to simplify and evaluate integrals.    Interpret and verify solutions by converting back to the original variable.    "
+},
+{
+  "id": "Formula3",
+  "level": "2",
+  "url": "ws-week-2-day-1.html#Formula3",
+  "type": "Remark",
+  "number": "3.1",
+  "title": "formulas.",
+  "body": " formulas      Expression  Trig Substitution  Angle Restriction  Identity                                                "
+},
+{
+  "id": "ws-week-2-day-1-3-1",
+  "level": "2",
+  "url": "ws-week-2-day-1.html#ws-week-2-day-1-3-1",
+  "type": "Worksheet Exercise",
+  "number": "3.1",
+  "title": "",
+  "body": "     What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-2-day-1-3-2",
+  "level": "2",
+  "url": "ws-week-2-day-1.html#ws-week-2-day-1-3-2",
+  "type": "Worksheet Exercise",
+  "number": "3.2",
+  "title": "",
+  "body": "     What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-2-day-1-4-1",
+  "level": "2",
+  "url": "ws-week-2-day-1.html#ws-week-2-day-1-4-1",
+  "type": "Worksheet Exercise",
+  "number": "3.3",
+  "title": "",
+  "body": "     What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-2-day-1-4-2",
+  "level": "2",
+  "url": "ws-week-2-day-1.html#ws-week-2-day-1-4-2",
+  "type": "Worksheet Exercise",
+  "number": "3.4",
+  "title": "",
+  "body": "     What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-2-day-1-5-1",
+  "level": "2",
+  "url": "ws-week-2-day-1.html#ws-week-2-day-1-5-1",
+  "type": "Worksheet Exercise",
+  "number": "3.5",
+  "title": "",
+  "body": "     What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-2-day-1-5-2",
+  "level": "2",
+  "url": "ws-week-2-day-1.html#ws-week-2-day-1-5-2",
+  "type": "Worksheet Exercise",
+  "number": "3.6",
+  "title": "",
+  "body": "     What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-3-day-1",
+  "level": "1",
+  "url": "ws-week-3-day-1.html",
+  "type": "Worksheet",
+  "number": "4",
+  "title": "Week 3: Partial Fractions Method",
+  "body": " Week 3: Partial Fractions Method      To be able to recognize when to use partial fractions.    To be able to break up one complicated fraction into multiple simpler fractions for integration.      formulas       Instructions: Evaluate each integral. Determine whether partial fractions is an appropriate technique, show all work, and clearly indicate your decomposition.        What was challenging? Note what you wonder or notice as you solve the problem.         What was challenging? Note what you wonder or notice as you solve the problem.          What was challenging? Note what you wonder or notice as you solve the problem.         What was challenging? Note what you wonder or notice as you solve the problem.          What was challenging? Note what you wonder or notice as you solve the problem.         What was challenging? Note what you wonder or notice as you solve the problem.     Whole Class Discussion     How do you know when a rational function can be decomposed using partial fractions?    Can you think of an integral where trying a different technique would be easier than partial fractions?      "
+},
+{
+  "id": "ws-week-3-day-1-2",
+  "level": "2",
+  "url": "ws-week-3-day-1.html#ws-week-3-day-1-2",
+  "type": "Objectives",
+  "number": "4",
+  "title": "",
+  "body": "   To be able to recognize when to use partial fractions.    To be able to break up one complicated fraction into multiple simpler fractions for integration.    "
+},
+{
+  "id": "Formula3_1",
+  "level": "2",
+  "url": "ws-week-3-day-1.html#Formula3_1",
+  "type": "Remark",
+  "number": "4.1",
+  "title": "formulas.",
+  "body": " formulas     "
+},
+{
+  "id": "ws-week-3-day-1-5",
+  "level": "2",
+  "url": "ws-week-3-day-1.html#ws-week-3-day-1-5",
+  "type": "Worksheet Exercise",
+  "number": "4.1",
+  "title": "",
+  "body": "     What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-3-day-1-6",
+  "level": "2",
+  "url": "ws-week-3-day-1.html#ws-week-3-day-1-6",
+  "type": "Worksheet Exercise",
+  "number": "4.2",
+  "title": "",
+  "body": "     What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-3-day-1-7",
+  "level": "2",
+  "url": "ws-week-3-day-1.html#ws-week-3-day-1-7",
+  "type": "Worksheet Exercise",
+  "number": "4.3",
+  "title": "",
+  "body": "     What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-3-day-1-8",
+  "level": "2",
+  "url": "ws-week-3-day-1.html#ws-week-3-day-1-8",
+  "type": "Worksheet Exercise",
+  "number": "4.4",
+  "title": "",
+  "body": "     What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-3-day-1-9",
+  "level": "2",
+  "url": "ws-week-3-day-1.html#ws-week-3-day-1-9",
+  "type": "Worksheet Exercise",
+  "number": "4.5",
+  "title": "",
+  "body": "     What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-3-day-1-10",
+  "level": "2",
+  "url": "ws-week-3-day-1.html#ws-week-3-day-1-10",
+  "type": "Worksheet Exercise",
+  "number": "4.6",
+  "title": "",
+  "body": "     What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-3-day-2",
+  "level": "1",
+  "url": "ws-week-3-day-2.html",
+  "type": "Worksheet",
+  "number": "5",
+  "title": "Week 3:??",
+  "body": " Week 3:??                 formulas      Instructions:          What was challenging? Note what you wonder or notice as you solve the problem.        What was challenging? Note what you wonder or notice as you solve the problem.           What was challenging? Note what you wonder or notice as you solve the problem.        What was challenging? Note what you wonder or notice as you solve the problem.           What was challenging? Note what you wonder or notice as you solve the problem.        What was challenging? Note what you wonder or notice as you solve the problem.         Whole Class Discussion                "
+},
+{
+  "id": "ws-week-3-day-2-2-1",
+  "level": "2",
+  "url": "ws-week-3-day-2.html#ws-week-3-day-2-2-1",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "           "
+},
+{
+  "id": "Formula3_2",
+  "level": "2",
+  "url": "ws-week-3-day-2.html#Formula3_2",
+  "type": "Remark",
+  "number": "5.1",
+  "title": "formulas.",
+  "body": " formulas    "
+},
+{
+  "id": "ws-week-3-day-2-3-1",
+  "level": "2",
+  "url": "ws-week-3-day-2.html#ws-week-3-day-2-3-1",
+  "type": "Worksheet Exercise",
+  "number": "5.1",
+  "title": "",
+  "body": "    What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-3-day-2-3-2",
+  "level": "2",
+  "url": "ws-week-3-day-2.html#ws-week-3-day-2-3-2",
+  "type": "Worksheet Exercise",
+  "number": "5.2",
+  "title": "",
+  "body": "    What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-3-day-2-4-1",
+  "level": "2",
+  "url": "ws-week-3-day-2.html#ws-week-3-day-2-4-1",
+  "type": "Worksheet Exercise",
+  "number": "5.3",
+  "title": "",
+  "body": "    What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-3-day-2-4-2",
+  "level": "2",
+  "url": "ws-week-3-day-2.html#ws-week-3-day-2-4-2",
+  "type": "Worksheet Exercise",
+  "number": "5.4",
+  "title": "",
+  "body": "    What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-3-day-2-5-1",
+  "level": "2",
+  "url": "ws-week-3-day-2.html#ws-week-3-day-2-5-1",
+  "type": "Worksheet Exercise",
+  "number": "5.5",
+  "title": "",
+  "body": "    What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-3-day-2-5-2",
+  "level": "2",
+  "url": "ws-week-3-day-2.html#ws-week-3-day-2-5-2",
+  "type": "Worksheet Exercise",
+  "number": "5.6",
+  "title": "",
+  "body": "    What was challenging? Note what you wonder or notice as you solve the problem.   "
+},
+{
+  "id": "ws-week-4-day-1",
+  "level": "1",
+  "url": "ws-week-4-day-1.html",
+  "type": "Worksheet",
+  "number": "6",
+  "title": "Week4:???",
+  "body": " Week4:???    "
+},
+{
+  "id": "ws-week-4-day-2",
+  "level": "1",
+  "url": "ws-week-4-day-2.html",
+  "type": "Worksheet",
+  "number": "7",
+  "title": "Week 4:????",
+  "body": " Week 4:????    "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
